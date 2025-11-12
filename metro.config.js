82537ba4e72978f6d22634d1950d6960335534f9
Jamie Birch <14055146+shirakaba@users.noreply.github.com>
2025-11-12 17:48:05 +0900

    support a sibling symlinked node_modules directory

--- metro.config.js
+++ metro.config.js
@@ -1,4 +1,5 @@
-const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
+const path = require("node:path");
+const { getDefaultConfig, mergeConfig } = require("@react-native/metro-config");
 
 /**
  * Metro configuration
@@ -6,6 +7,13 @@ const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
  *
  * @type {import('@react-native/metro-config').MetroConfig}
  */
-const config = {};
+const config = {
+  // While the original react-native-fiddle-repro template brings its own
+  // node_modules, derived Fiddles do not, and so we symlink back to the
+  // template's node_modules. Thus, below we adjust the Metro config to support
+  // a sibling symlinked node_modules directory.
+  resolver: { unstable_enableSymlinks: true },
+  watchFolders: [path.resolve(__dirname, "node_modules")],
+};
 
 module.exports = mergeConfig(getDefaultConfig(__dirname), config);
