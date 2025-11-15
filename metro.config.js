3de259003bcb944051c4a4533eb3591eee200bbc
Jamie Birch <14055146+shirakaba@users.noreply.github.com>
2025-11-16 00:01:27 +0900

    stop printing config

--- metro.config.js
+++ metro.config.js
@@ -4,7 +4,6 @@ const { getDefaultConfig, mergeConfig } = require("@react-native/metro-config");
 
 const symlinkedNodeModules = path.resolve(__dirname, "node_modules");
 const realNodeModules = fs.realpathSync(symlinkedNodeModules);
-console.log("realNodeModules", realNodeModules);
 
 /**
  * Metro configuration
@@ -76,5 +75,4 @@ config.transformer.babelTransformerPath =
     realNodeModules
   );
 
-console.log("final config", config);
 module.exports = config;
