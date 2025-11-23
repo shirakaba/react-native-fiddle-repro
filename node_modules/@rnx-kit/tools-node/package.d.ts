0026b203e12efd7cdc59fbb7e8da0926eda6d08b
Jamie Birch <14055146+shirakaba@users.noreply.github.com>
2025-11-24 01:15:39 +0900

    install @rnx-kit/cli and @rnx-kit/metro-config

---
+++ node_modules/@rnx-kit/tools-node/package.d.ts
@@ -0,0 +1,15 @@
+export {
+  findPackage,
+  findPackageDependencyDir,
+  findPackageDir,
+  parsePackageRef,
+  readPackage,
+  resolveDependencyChain,
+  writePackage,
+} from "./lib/package";
+export type {
+  FindPackageDependencyOptions,
+  PackageManifest,
+  PackagePerson,
+  PackageRef,
+} from "./lib/package";
---
+++ node_modules/@rnx-kit/tools-react-native/platform.d.ts
@@ -0,0 +1,8 @@
+export {
+  expandPlatformExtensions,
+  getAvailablePlatforms,
+  getAvailablePlatformsUncached,
+  parsePlatform,
+  platformExtensions,
+} from "./lib/platform";
+export type { AllPlatforms } from "./lib/platform";
